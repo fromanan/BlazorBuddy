6b4757a9bb8acc816efd5cd180c4cda45575f365
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-11-06 12:23:21 -0500

    Added Extensions classes

---
+++ Application/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (T element in enumerable)
+        {
+            action(element);
+        }
+    }
+    
+    public static async Task EachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action)
+    {
+        foreach (T element in enumerable)
+        {
+            await action(element);
+        }
+    }
+}
