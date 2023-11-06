07aa0e671c71f61f41d09ef2db9a68bbd63025bb
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-11-06 12:23:56 -0500

    Cleaned up comments in Program.cs

--- Application/Program.cs
+++ Application/Program.cs
@@ -2,7 +2,7 @@ using Application.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -10,11 +10,12 @@ builder.Services.AddBlazorContextMenu();
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
    app.UseExceptionHandler("/Error");
-   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   // The default HSTS value is 30 days
+   // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts
    app.UseHsts();
 }
 
