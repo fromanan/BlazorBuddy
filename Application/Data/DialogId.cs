6ce5723ae006c5628e44548b73c68c32b4eb8d14
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-11-06 12:34:19 -0500

    Changed Rename id to rename-dialog instead of modal

--- Application/Data/DialogId.cs
+++ Application/Data/DialogId.cs
@@ -3,5 +3,5 @@
 public static class DialogId
 {
     public static readonly string Save = "save-modal";
-    public static readonly string Rename = "rename-modal";
+    public static readonly string Rename = "rename-dialog";
 }
