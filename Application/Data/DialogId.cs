8af8a438c933d5973afeef30b6713e2b5c132863
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-11-06 12:34:57 -0500

    Added ImportDialog

--- Application/Data/DialogId.cs
+++ Application/Data/DialogId.cs
@@ -4,4 +4,5 @@ public static class DialogId
 {
     public static readonly string Save = "save-modal";
     public static readonly string Rename = "rename-dialog";
+    public static readonly string Import = "import-dialog";
 }
