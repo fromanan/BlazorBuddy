4fd6b9aee27b4c3010077245397b770e853e49f2
Tony Froman <froman.tony@aoins.com>
2023-11-07 14:58:00 -0500

    Migrations

---
+++ Application/Migrations/20231107165315_InitialMigration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    LastChange = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    SessionType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Windows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    IsCurrentWindow = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsIncognito = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Window = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Windows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Windows_Sessions_Window",
+                        column: x => x.Window,
+                        principalTable: "Sessions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tabs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    Tab = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tabs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tabs_Windows_Tab",
+                        column: x => x.Tab,
+                        principalTable: "Windows",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tabs_Tab",
+                table: "Tabs",
+                column: "Tab");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Windows_Window",
+                table: "Windows",
+                column: "Window");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tabs");
+
+            migrationBuilder.DropTable(
+                name: "Windows");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
---
+++ Application/Migrations/20231107192649_NamingColumns.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class NamingColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCurrentWindow",
+                table: "Windows");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Windows",
+                newName: "title");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Windows",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "IsIncognito",
+                table: "Windows",
+                newName: "is_incognito");
+
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Tabs",
+                newName: "url");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Tabs",
+                newName: "title");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Tabs",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Sessions",
+                newName: "title");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Sessions",
+                newName: "created");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Sessions",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "SessionType",
+                table: "Sessions",
+                newName: "session_type");
+
+            migrationBuilder.RenameColumn(
+                name: "LastChange",
+                table: "Sessions",
+                newName: "last_change");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Windows",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Windows",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "is_incognito",
+                table: "Windows",
+                newName: "IsIncognito");
+
+            migrationBuilder.RenameColumn(
+                name: "url",
+                table: "Tabs",
+                newName: "Url");
+
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Tabs",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Tabs",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Sessions",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "created",
+                table: "Sessions",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Sessions",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "session_type",
+                table: "Sessions",
+                newName: "SessionType");
+
+            migrationBuilder.RenameColumn(
+                name: "last_change",
+                table: "Sessions",
+                newName: "LastChange");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCurrentWindow",
+                table: "Windows",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
